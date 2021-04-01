206803bcd1984babdd8725926b157ca356a7bda0
Polina Chubenko <chubenko.pn@phystech.edu>
2021-04-01 23:50:04 +0300

    working with states and creating an USEREVENT for interval timer

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ BACKGROUND_COLOR = WHITE
 
 
 class Pet(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, state="good"):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("imgs/good.png").convert()
+        self.satiety = 80
+        self.image_path = "imgs/{}.png".format(state)
+        self.image = pygame.image.load(self.image_path).convert()
         self.image.set_colorkey(WHITE, RLEACCEL)
         self.rect = self.image.get_rect()
         self.rect.center = (WIN_WIDTH / 2, WIN_WIDTH / 2)
+
+    def update(self, new_state):
+        self.image_path = "imgs/{}.png".format(new_state)
+        self.image = pygame.image.load(self.image_path).convert()
 
 
 if __name__ == '__main__':
@@ -29,16 +35,19 @@ if __name__ == '__main__':
     pygame.display.set_caption("Tamagotchi")
     clock = pygame.time.Clock()
 
-    player = Pet()
-    all_sprites = pygame.sprite.Group()
-    all_sprites.add(player)
+    states = "bad"
+    player = Pet(states)
 
+    pygame.time.set_timer(pygame.USEREVENT, 2000)
     running = True
     while running:
-        # Держим цикл на правильной скорости
         clock.tick(FPS)
-        # Ввод процесса (события)
         for event in pygame.event.get():
+            if event.type == pygame.USEREVENT:
+                if states == "bad":
+                    states = "good"
+                else:
+                    states = "bad"
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
@@ -46,9 +55,8 @@ if __name__ == '__main__':
                 running = False
 
         screen.fill(BACKGROUND_COLOR)
-
-        for entity in all_sprites:
-            screen.blit(entity.image, entity.rect)
+        player.update(states)
+        screen.blit(player.image, player.rect)
         pygame.display.flip()
 
     pygame.quit()
