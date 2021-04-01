d06b858c2952978ffa69da52bb558b46a003078b
Polina Chubenko <chubenko.pn@phystech.edu>
2021-04-01 22:40:04 +0300

    Pet created and imgs are uploaded

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import pygame
+from pygame import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+WIN_WIDTH = 500
+WIN_HEIGHT = 500
+FPS = 60
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
+
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BACKGROUND_COLOR = WHITE
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Pet(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("imgs/good.png").convert()
+        self.image.set_colorkey(WHITE, RLEACCEL)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIN_WIDTH / 2, WIN_WIDTH / 2)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.init()
+    pygame.mixer.init()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    screen = pygame.display.set_mode(DISPLAY)
+    pygame.display.set_caption("Tamagotchi")
+    clock = pygame.time.Clock()
+
+    player = Pet()
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(player)
+
+    running = True
+    while running:
+        # Держим цикл на правильной скорости
+        clock.tick(FPS)
+        # Ввод процесса (события)
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+            elif event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(BACKGROUND_COLOR)
+
+        for entity in all_sprites:
+            screen.blit(entity.image, entity.rect)
+        pygame.display.flip()
+
+    pygame.quit()
