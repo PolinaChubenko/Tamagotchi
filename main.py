7116e872ce58f6dd21c184adcac61c2261251a4c
Polina Chubenko <chubenko.pn@phystech.edu>
2021-04-03 13:42:58 +0300

    creating multiple USEREVENT for interval timer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import *
+from set_interval import setInterval
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 500
@@ -10,6 +11,13 @@ GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BACKGROUND_COLOR = WHITE
+
+ON_SATIETY_LOWERS = 1
+ON_MOOD_CHANGE = 2
+
+
+def set_timer(event_obj, interval):
+    return setInterval(func=lambda x: pygame.event.post(x), sec=interval, args=[event_obj])
 
 
 class Pet(pygame.sprite.Sprite):
@@ -38,21 +46,34 @@ if __name__ == '__main__':
     states = "bad"
     player = Pet(states)
 
-    pygame.time.set_timer(pygame.USEREVENT, 2000)
+    # pygame.time.set_timer(pygame.USEREVENT, 2000)
+    SatietyEvent = pygame.event.Event(pygame.USEREVENT, MyOwnType=ON_SATIETY_LOWERS)
+    MoodEvent = pygame.event.Event(pygame.USEREVENT, MyOwnType=ON_MOOD_CHANGE)
+    myIntervalHandle1 = set_timer(SatietyEvent, 2)
+    myIntervalHandle2 = set_timer(MoodEvent, 2)
     running = True
     while running:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.USEREVENT:
-                if states == "bad":
-                    states = "good"
-                else:
-                    states = "bad"
+                if event.MyOwnType == ON_MOOD_CHANGE:
+                    print("Mood change")
+                    if states == "bad":
+                        states = "good"
+                    else:
+                        states = "bad"
+                elif event.MyOwnType == ON_SATIETY_LOWERS:
+                    player.satiety -= 5
+                    print("Satiety change:", player.satiety)
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+                    myIntervalHandle1.stop()
+                    myIntervalHandle2.stop()
             elif event.type == pygame.QUIT:
                 running = False
+                myIntervalHandle1.stop()
+                myIntervalHandle2.stop()
 
         screen.fill(BACKGROUND_COLOR)
         player.update(states)
